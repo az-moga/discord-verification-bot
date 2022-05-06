faaeee8f53b9102386b6828d497ff9c70db253ce
Nikola Velichkov <nevelichkoff@gmail.com>
2022-05-06 07:44:55 +0300

    Fix feed service bootstrap

--- src/services/FeedService.ts
+++ src/services/FeedService.ts
@@ -5,6 +5,11 @@ export class FeedService {
     constructor(
         client: Client
     ) {
+        // TODO need to dispose this
+        client.on('ready', this.bootstrap.bind(this));
+    }
+
+    async bootstrap(client: Client<true>) {
         let channel = client.channels.cache.find(c => c.id === process.env.FEED_CHANNEL_ID);
         if(this.channel?.isText() !== true) {
             throw new Error("Unsupported feed channel");
